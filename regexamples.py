4525ffce53d464dcb5e8497852c57a2f7ed4493d
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2017-08-22 21:24:40 -0400

    Web scraping first commit

--- regexamples.py
+++ regexamples.py
@@ -1,3 +1,9 @@
 import re
-try = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-str = try.search('my number is 667-212-6903')
+
+try1 = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+str = try1.search('my number is 667-212-6903')
+print str.group()
+
+try2 = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+str2 = try2.search('my number is 667-212-6903')
+print str2.group(1)
---
+++ scraping.py
@@ -0,0 +1,4 @@
+import webbrowser, sys
+if len(sys.argv) > 1:
+	#Use join to make multiple strings into 1 string
+	address = ' '.join(sys.argv[1:])
