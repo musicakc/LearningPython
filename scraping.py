35a6da13f83968e2d0394a7aa0dd973b2cdd86c3
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2017-08-22 21:41:20 -0400

    First task in web scraping done

--- scraping.py
+++ scraping.py
@@ -1,4 +1,15 @@
-import webbrowser, sys
+import webbrowser, sys, pyperclip
+
+'''
+First task: open a web link using webbrowser library
+Example usage: 
+python scraping.py The Marylander Apartment Homes, Baltimore Maryland, Saint Paul Street, Baltimore, MD
+'''
+
 if len(sys.argv) > 1:
 	#Use join to make multiple strings into 1 string
 	address = ' '.join(sys.argv[1:])
+else:
+	#Use pyperclip to get address from clipboard
+	address = pyperclip.paste()
+webbrowser.open('https://www.google.com/maps/place/' + address)
